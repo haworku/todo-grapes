3bcf33c0e4447b5e09043f1a6e57f3ce3a8ae533
haworku <haworku@gmail.com>
2017-02-27 16:25:31 -0800

    intervening in active record errors

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,4 +3,12 @@ class API < Grape::API
   format :json
   version 'v1', using: :path
   mount Todo::Data
+
+   rescue_from ActiveRecord::RecordNotFound do |e|
+	  error_response(message: e.message, status: 404)
+	end
+	
+   rescue_from ActiveRecord::RecordInvalid do |e|
+      error_response(message: e.message, status: 422)
+    end
 end
