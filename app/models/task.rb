41df0ec9bcfde56f9dee3ae67c7a9f5f9c740808
haworku <haworku@gmail.com>
2017-03-02 11:31:01 -0600

    Tasks can be unassigned -users_id can be null

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ApplicationRecord
 	belongs_to :user
-  validates :name, presence: :true
-  validates :complete, presence: :true
+  # validates :name, presence: :true
+  # validates :complete, presence: :true
+  # validates :users_id, presence: :true
+
 end
