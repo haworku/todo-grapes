349ede6f94337a85cd1352e5645c05bf548f5c56
haworku <haworku@gmail.com>
2017-03-02 19:16:15 -0600

    Clarify bugs and fix seed

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
 	belongs_to :user
   validates :name, presence: :true
   validates :users_id, numericality: { only_integer: true }
-	validates :complete, inclusion: { in: [true, false], message: "%{value} must be 'true' or 'false'." }
+	validates :complete, inclusion: { in: [true, false], message: "%{value} must be 'true' or 'false'." } #BUG: truthiness slippery slope
 	validate :due_in_future, unless: :persisted?
 	validate :valid_date_check
 
@@ -13,7 +13,7 @@ class Task < ApplicationRecord
 	end
 
 	def valid_date_check
-		if due_date.is_a?(Date) == false
+		if due_date.present? && due_date.is_a?(Date) == false
       errors.add(:due_date, 'must be a valid date or else empty.')
 		end
 	end
