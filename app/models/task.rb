cf609e09be5e9f528b6ab73e4035acab5c106e01
haworku <haworku@gmail.com>
2017-03-02 19:07:01 -0600

    Update task validations -must have name, users id as integer, complete as bool

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
 	belongs_to :user
-  # validates :name, presence: :true
-  # validates :complete, presence: :true
-  # validates :users_id, presence: :true
+  validates :name, presence: :true
+  validates :users_id, numericality: { only_integer: true }
+	validates :complete, inclusion: { in: [true, false], message: "%{value} must be 'true' or 'false'." }
 
 end
