4b92cb3c0fc5a85caecaced6bd5f153a39cb1604
haworku <haworku@gmail.com>
2017-03-03 11:13:11 -0600

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ user2 = User.create(name: 'you')
 #Must use due_dates in the future
 Task.create([
 	{name: 'buy red grapes', users_id: user2.id, due_date: '2017-09-02'}, 
-	{name: 'buy purple grapes', users_id: user1.id, complete: true, due_date: '2017-06-01'}, 
+	{name: 'buy purple grapes', users_id: user1.id, complete: false, due_date: '2017-06-01'}, 
 	{name: 'yum grape skittles', users_id: user1.id, complete: true},
-	{name: 'grape freezies for the summer', users_id: user1.id, due_date: '2017-03-07', complete: true},
+	{name: 'grape freezies for the summer', users_id: user1.id, due_date: '2017-03-07', complete: false},
 	{name: 'grape juice', users_id: user2.id, complete: true},    
 ])
